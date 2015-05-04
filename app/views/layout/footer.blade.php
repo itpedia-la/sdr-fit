cd4c3a3cb944d2ddfd9cf4ea610861cfabddfcce
Somwang Souksavatd <dsouksavatd@gmail.com>
2015-05-04 14:07:06 +0700

    update

--- app/views/layout/footer.blade.php
+++ app/views/layout/footer.blade.php
@@ -1,7 +1,7 @@
 @if( Auth::id() ) 
 <hr style="margin:50px 0px 0px 0px"/><p style="color:#666;  font-size:10px" align="center">Copyright &copy; 2015 - Miraclelao Advertising Sole.,Ltd | Developed by: IT Pedia Sole.,Ltd (+856 20 5999 8848)</p> 
 @else
-<p style="color:#666; line-height:2em; font-size:10px" align="center">Copyright &copy; 2015 - Miraclelao Advertising Sole.,Ltd <br/> Developed by: IT Pedia Sole.,Ltd (+856 20 5999 8848)</p>
+<p style="color:#666; line-height:2em; font-size:10px" align="center">Copyright &copy; 2015 - {{ Config::get('app.title') }}  <br/> Developed by: IT Pedia Sole.,Ltd</p>
 @endif
 </div>
 
